f171c440b7f878d9595df49113ed6f4ddfba57d8
Maxthod <maxthod@pm.me>
2021-11-24 00:17:01 -0500

    "master"

---
+++ src/test/java/com/duvalhub/dummyspringboot/DummySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duvalhub.dummyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
